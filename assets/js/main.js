8c3e1df0a7118b30bf176996dc9eb881100fa31d
Mathieu BAGUE <mathieu.bague@rte-france.com>
2020-05-07 17:22:54 +0200

    Fix License
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,12 @@
  * Author: BootstrapMade.com
  * License: https://bootstrapmade.com/license/
  */
+
 !(function($) {
   "use strict";
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
-  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
+  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto, a', function(e) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       e.preventDefault();
       var target = $(this.hash);
